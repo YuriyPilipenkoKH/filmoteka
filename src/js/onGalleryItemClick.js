30cf05135fba3a50a0d54d0d69dc412d49e13ce1
TarasVarvarych <varvarychtaras@gmail.com>
2023-04-08 23:50:49 +0300

    Update onGalleryItemClick.js

--- src/js/onGalleryItemClick.js
+++ src/js/onGalleryItemClick.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const BASE_URL = 'https://api.themoviedb.org/';
 const API_KEY = '90c7ff0c6a89140d8ec65b5296dfcca2';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const DEFAULT_POSTER_URL =
+  'https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie.jpg';
 
 export async function onGalleryItemClick(e) {
   if (e.target.nodeName !== 'IMG') {
