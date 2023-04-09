831d5f3fed2e5cce40bdb873eeed06f004032aba
TarasVarvarych <varvarychtaras@gmail.com>
2023-04-09 10:22:57 +0300

    123

--- src/js/renderFilms.js
+++ src/js/renderFilms.js
@@ -1,7 +1,4 @@
-import axios from 'axios';
 import { refs } from './refs';
-const BASE_URL = 'https://api.themoviedb.org/';
-const API_KEY = '90c7ff0c6a89140d8ec65b5296dfcca2';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const DEFAULT_POSTER_URL =
   'https://motivatevalmorgan.com/wp-content/uploads/2016/06/default-movie.jpg';
@@ -54,56 +51,5 @@ export function renderMoviesMarkup(data) {
     })
     .join('');
 
-  refs.galleryList.addEventListener('click', onGalleryItemClick);
-  async function onGalleryItemClick(e) {
-    if (e.target.nodeName !== 'IMG') {
-      return;
-    }
-    const id = e.target.dataset.id;
-    const response = await axios(`${BASE_URL}3/movie/${id}?api_key=${API_KEY}`);
-    console.log(response);
-    const {
-      poster_path,
-      title,
-      vote_average,
-      vote_count,
-      popularity,
-      original_title,
-      genres,
-      overview,
-    } = response.data;
-    const posterUrl = poster_path
-      ? `${IMG_URL}${poster_path}`
-      : DEFAULT_POSTER_URL;
-    const movieMarkup = `<div class="modal__card-thumb">
-    <img class="modal__image" src="${posterUrl}" alt="film-image" />
-  </div>
-  <div class="modal__info">
-    <h2 class="modal__title">${title}</h2>
-    <div class="modal__list-box">
-      <ul class="modal__list modal__list--first-list">
-        <li>Vote / Votes</li>
-        <li>Popularity</li>
-        <li>Original Title</li>
-        <li>Genre</li>
-      </ul>
-      <ul class="modal__list">
-        <li>${vote_average} / ${vote_count}</li>
-        <li>${popularity}</li>
-        <li>${original_title}</li>
-        <li>${genres.map(genre => genre.name).join(', ')}</li>
-      </ul>
-    </div>
-    <div>	
-      <h3 class="modal__subtitle">About</h3>	
-      <p class="modal__text">${overview}</p>	
-    </div>	
-    <div class="modal__buttons">	
-      <button class="modal__btn" type="button">add to Watched</button>	
-      <button class="modal__btn" type="button">add to queue</button>	
-    </div>	
-  </div>`;
-    refs.movieModal.innerHTML = movieMarkup;
-  }
   refs.galleryList.innerHTML = markup;
 }
