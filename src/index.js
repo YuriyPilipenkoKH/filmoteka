9575a38ecf035c67f32a0abae6572cb661217367
Ihor849 <333marchelo333@gmail.com>
2023-04-12 11:39:03 +0300

    11.39

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './sass/index.scss';
-import './js/apiSevice';
+import './js/apiServiceMovies';
 import './js/renderFilms';
 import { renderMoviesMarkup } from './js/renderFilms';
 import { refs } from './js/refs';
 import { fetchTrendingMovies } from './js/fetchTrendingMovies';
-import { onFormSubmit } from './js/onSearchFormSubmit';
+// import { onFormSubmit } from './js/onSearchFormSubmit';
 import { getMoviesGenres } from './js/getMoviesGenres';
 import { onGalleryItemClick } from './js/onGalleryItemClick';
 import './js/modalTeam';
@@ -12,6 +12,9 @@ getMoviesGenres();
 fetchTrendingMovies().then(renderMoviesMarkup);
 refs.moviesSearchForm.addEventListener('submit', onFormSubmit);
 refs.galleryList.addEventListener('click', onGalleryItemClick);
+
+import './js/onSearchByTrend';
+import './js/onSearchByName';
 
 import './js/modalFilm';
 import './js/scrolled';
