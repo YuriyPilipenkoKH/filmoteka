dff3327934b8d06cfb1c0a4103ba4fa04edf9f49
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-12 15:37:48 +0300

    Update onSearchLibrary.js

--- src/js/onSearchLibrary.js
+++ src/js/onSearchLibrary.js
@@ -1,4 +1,11 @@
+import { fetchMoviesByName } from './fetchMoviesByName';
+import { renderMoviesMarkup } from './renderFilms';
+import { startLoader, stopLoader } from './loader';
 import { refs } from './refs';
+import { MoviesServiceByName } from './apiServiceMovies';
+import { onFormSubmit} from './onSearchByName';
+
+import {onFormSubmit, onSearchByName} from './onSearchByName'
 // import {onSearchByName} from './js/fetchMoviesByName'
 
  refs.librarySubmit.addEventListener('submit', onSearchLibrary);
@@ -8,5 +15,6 @@ console.log();
 function onSearchLibrary(e) {
     e.preventDefault
     window.location.href = './index.html'
+    onFormSubmit()
  
 }
