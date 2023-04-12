b31219d90fbd10ca0dd8a855bdee742a24b6fea0
Ihor849 <333marchelo333@gmail.com>
2023-04-12 11:21:59 +0300

    onSearchByTrend

---
+++ src/js/on SearchByTrend.js
@@ -0,0 +1,27 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+import { MoviesService } from './apiServiceMovies';
+// import { fetchTrendingMovies } from './fetchTrendingMovies';
+import { renderMoviesMarkup } from './renderFilms';
+
+const moviesService = new MoviesService({
+  searchQuery: '',
+  page: 1,
+});
+
+const container = document.getElementById('pagination');
+
+const pagination = new Pagination('pagination', {
+  totalItems: 500,
+  itemsPerPage: 10,
+  visiblePages: 5,
+  page: 1,
+});
+pagination.on('beforeMove', event => {
+  const currentPage = event.page;
+
+  if (currentPage) {
+    moviesService.page = currentPage;
+    moviesService.fetchTrendingMovies().then(renderMoviesMarkup);
+  }
+});
