bc758e1ae12f9b576f29893b5c5a6b433dfd7d65
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-14 13:32:24 +0300

    fix

--- src/js/onSearchByName.js
+++ src/js/onSearchByName.js
@@ -1,11 +1,11 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
-
 import { renderMoviesMarkup } from './renderFilms';
 import { startLoader, stopLoader } from './loader';
 import { refs } from './refs';
-import { MoviesServiceByName } from './apiServiceMovies';
+import { MoviesServiceByName, totalFilmsOnSearch } from './apiServiceMovies';
+import { startLoader } from './loader';
 
 refs.moviesSearchForm.addEventListener('submit', onFormSubmit);
 
@@ -31,21 +31,31 @@ export async function onFormSubmit(e) {
   } else {
     startLoader();
     try {
-      moviesServiceByName.fetchMoviesByName().then(renderMoviesMarkup);
-
-      const pagination = new Pagination('pagination', {
-        totalItems: 500,
-        itemsPerPage: 10,
-        visiblePages: 5,
-        page: 1,
-      });
-      pagination.on('beforeMove', event => {
-        const currentPage = event.page;
-        moviesServiceByName.page = currentPage;
-        moviesServiceByName.fetchMoviesByName().then(renderMoviesMarkup);
-      });
+      moviesServiceByName
+        .fetchMoviesByName()
+        .then(getPageQuantity)
+        .then(renderMoviesMarkup);
+      refs.isTrendingMoviesActive = false;
     } catch (error) {
       console.log(error);
     }
   }
+}
+
+function getPageQuantity(response) {
+  totalFilmsOnSearch = response.data.total_results;
+  const pagination = new Pagination('pagination', {
+    totalItems: totalFilmsOnSearch,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    page: 1,
+  });
+  pagination.on('beforeMove', event => {
+    const currentPage = event.page;
+    moviesServiceByName.page = currentPage;
+    startLoader();
+    moviesServiceByName.fetchMoviesByName().then(renderMoviesMarkup);
+  });
+
+  return response;
 }
