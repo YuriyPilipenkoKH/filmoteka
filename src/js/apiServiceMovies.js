25ea924e879ccca19e1e06741998a47976afed42
Ihor849 <333marchelo333@gmail.com>
2023-04-14 13:55:02 +0300

    Перезаливка

--- src/js/apiServiceMovies.js
+++ src/js/apiServiceMovies.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { startLoader, stopLoader } from './loader';
-
+///=========== ПЕРЕЗАЛИВКА
 
 let totalFilmsOnSearch = 0;
 
